ec7487229b26fbd8cf0c6ed6e52e7fd1d72ac29b
Alex <alex@ctmlabs.io>
2014-01-20 21:51:23 +0000

    update

--- edison_hive/node-collector/web.js
+++ edison_hive/node-collector/web.js
@@ -1,15 +1,37 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var flash 	 = require('connect-flash');
+var passport = require('passport');
 var app = express();
+module.exports = app;
+//var port = process.env.PORT || 5000;
+require('./config/passport.js')(passport); // pass passport for configuration
 
-app.use(logfmt.requestLogger());
+app.configure(function()	{
+	//app.use(logfmt.requestLogger()); //log requests
+	app.use(express.cookieParser()); //read cookies needed for auth
+	app.use(express.bodyParser()); //get information from html forms
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+	app.set('view engine', 'ejs'); //set up ejs templating
+
+	//required for passort
+	app.use(express.session({secret: 'benleftlabsandwenttothehometeamnowwehaveboyne'}));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.set('port', process.env.PORT || 5000);
+	app.use(flash()); 
+})
+
+
+require('./app/routes.js')(app, passport);
+
+
+
+
+
+
+app.listen(app.get('port'), function() {
+  //console.log("Listening on " + app.get('port'));
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
